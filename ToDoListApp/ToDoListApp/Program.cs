29919c28d9795e028ada4c5503cc61245d2943ac
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-16 21:59:18 -0400

    To do list app created + Coded
    To do list app created from the github folder.
    
    Program.cs:
    userprompt for how to use the app is coded via writeLine

---
+++ ToDoListApp/ToDoListApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//user prompt for app instruction
+Console.WriteLine("To-Do List: ");
+System.Console.WriteLine("Enter 1-4 to interact with the list.");
+Console.WriteLine("Enter 1 to check the list.");
+Console.WriteLine("Enter 2 to add to the list.");
+Console.WriteLine("Enter 3 to remove a task from the list.");
+Console.WriteLine("Enter 4 to exit the program.");
+
