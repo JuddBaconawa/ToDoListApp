6aaebc8454ce382eb1e3006f4854964138ca500d
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-17 22:43:21 -0400

    Update Program.cs
    To do list menu set up
    
    Console.Readline for the user input set up
    
    switch statement for option started but not finished.

--- ToDoListApp/ToDoListApp/Program.cs
+++ ToDoListApp/ToDoListApp/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+//creation of a list named task for tasks to be stored in
+List<string> task = new List<string>();
+bool running = true; //ensures that the code is running
+
 //user prompt for app instruction
-Console.WriteLine("To-Do List: ");
-System.Console.WriteLine("Enter 1-4 to interact with the list.");
-Console.WriteLine("Enter 1 to check the list.");
-Console.WriteLine("Enter 2 to add to the list.");
-Console.WriteLine("Enter 3 to remove a task from the list.");
-Console.WriteLine("Enter 4 to exit the program.");
+while (running) {
+	Console.WriteLine("To-Do List: ");
+	System.Console.WriteLine("Enter 1-4 to interact with the list.");
+	Console.WriteLine("Enter 1 to check the list.");
+	Console.WriteLine("Enter 2 to add to the list.");
+	Console.WriteLine("Enter 3 to remove a task from the list.");
+	Console.WriteLine("Enter 4 to exit the program.");
+	Console.WriteLine("Choose an option: ");
+
+	string choice = Console.ReadLine(); //reads inputs by the user
+
+	//switch statement for the main menu options
+	switch (choice) {
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		default:
+	}
+}
+
 
